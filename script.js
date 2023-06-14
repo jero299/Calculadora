9668ca430f475207b9e81e234b5163d23d57276d
Jero <jmsolis9922@gmail,com>
2023-06-14 15:05:21 -0600

    se agrego una funcion para poder usar las teclas numericas

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ function calculate() {
     let result = document.getElementById("result").value;
     let answer = eval(result); 
     document.getElementById("result").value = answer;
-  }  
+  }
+//funcion para usar las teclas numericas
+function teclas(event) {
+    // Obtiene el código de la tecla presionada
+    let tecla = event.key || event.which;
+
+    // Obtén el campo de texto
+      var input = document.getElementById("result");
+  
+    // Agrega el número al valor actual del campo de texto
+      input.value += tecla;
+  
+    // Previene que se ejecute el comportamiento por defecto del evento de la tecla presionada
+      event.preventDefault();
+    }
+  
+  
