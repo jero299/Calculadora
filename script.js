825dba3cbfd489a8c6589d6534440e1660a9b28c
Jero <jmsolis9922@gmail,com>
2023-06-27 17:52:40 -0600

    se agrego una funcion que evitar que el usuario pueda usar dos operadores repetido para evitar errores

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-//funcion para agregar operaciones
+// Función para agregar operaciones
 function appendToResult(value) {
   const input = document.getElementById("result");
   const currentValue = input.value;
   
-  if (value === '.' && !validarPunto()) {
-    return; // No se permite agregar el punto decimal
+  // Validar operadores duplicados
+  const lastChar = currentValue[currentValue.length - 1];
+  if (isOperator(lastChar) && isOperator(value)) {
+    return; // No se permite agregar operador duplicado
   }
   
   input.value = currentValue + value;
 }
 
-//funcion para realizar el calculo 
+// Función auxiliar para verificar si un carácter es un operador (+, -, *, /)
+function isOperator(char) {
+  return char === '+' || char === '-' || char === '*' || char === '/' || char === '.';
+}
+
+// Función para realizar el cálculo
 function calculate() {
-    let result = document.getElementById("result").value;
-    let answer = eval(result); 
-    document.getElementById("result").value = answer;
+  let result = document.getElementById("result").value;
+  let answer = eval(result);
+  document.getElementById("result").value = answer;
+}
 
-  }
-
-//funcion para evitar escribir letras
+// Función para evitar escribir letras
 function restringirTeclas(event) {
   // Obtiene el valor actual del input
   let value = event.target.value;
   
-  // Elimina todos los caracteres que no sean números, suma, resta, multiplicación o división
+  // Elimina todos los caracteres que no sean números, operadores, o el punto decimal
   let newValue = value.replace(/[^0-9+.\-\/*]/g, "");
   
   // Actualiza el valor del input con el nuevo valor restringido
   event.target.value = newValue;
-}
-//TODO crear una variable que cuente los puntos y reinicie el contador para poner numero decimales
-function validarPunto() {
-  const value = document.getElementById("result").value;
-  return !(value.includes('.') && /\.\d*$/.test(value));
 }
 
 // Función para borrar el contenido del campo de texto
@@ -40,21 +41,22 @@ function borrar() {
   let input = document.getElementById("result");
   input.value = "";
 }
- //funcion para borrar usando el boton C Y usando backspace
- document.addEventListener("keydown", function(event){
+
+// Evento para borrar usando el botón "C" y la tecla "Backspace"
+document.addEventListener("keydown", function(event) {
   if (event.key === "Backspace"){
     borrar();
     event.preventDefault();
   }
- })
- // funcion para obtener el resultado presionando enter
- document.addEventListener("keydown", function(event){
+});
+
+// Evento para obtener el resultado al presionar la tecla "Enter"
+document.addEventListener("keydown", function(event) {
   if (event.key === "Enter") {
     calculate();
     event.preventDefault();
   }
 });
-
 
 
 
